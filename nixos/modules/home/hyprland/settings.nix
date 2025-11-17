e09c35474adbad5cbc2124028e2df197e758a6c6
Nathan <nathanscott1013@hotmail.com>
2025-11-17 06:06:19 -0500

    Update settings.nix borders

--- nixos/modules/home/hyprland/settings.nix
+++ nixos/modules/home/hyprland/settings.nix
@@ -4,6 +4,13 @@
   wayland.windowManager.hyprland.settings = {
     general = {
       "$mainMod" = "SUPER";
+      layout = "dwindle";
+      gaps_in = 6;
+      gaps_out = 12;
+      border_size = 2;
+      "col.active_border" = "rgb(98971A) rgb(CC241D) 45deg";
+      "col.inactive_border" = "0x00000000";
+      no_border_on_floating = false;
     };
 
     decoration = {
